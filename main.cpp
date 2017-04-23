4e422b43367c42694789dd59ad2ba1c3c1d02c6d
wojtałke <wojtalkee@prokonto.pl>
2017-04-23 22:26:13 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int DarthVader = 808;
+  while(DarthVader >= 1)
+  {
+    cout << DarthVader << endl;
+    DarthVader--;
+    }
+    return 0;
+    }
